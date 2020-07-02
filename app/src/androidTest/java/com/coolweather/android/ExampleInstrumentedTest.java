bbecc4e01eacfd147370c66e99098187af8b81ca
FangX-ob <1911421582@qq.com>
2020-07-02 20:46:25 +0800

    最新提交

--- app/src/androidTest/java/com/coolweather/android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/coolweather/android/ExampleInstrumentedTest.java
@@ -1,27 +1,13 @@
 package com.coolweather.android;
 
-import android.content.Context;
-
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
+import android.app.Application;
+import android.test.ApplicationTestCase;
 
 /**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("com.coolweather.android", appContext.getPackageName());
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
     }
 }
